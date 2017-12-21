950db1c0e3ad4b4e734abc55b792b7c7c208e305
Beth <bethwambuimuniu@gmail.com>
2017-12-21 10:11:43 +0300

    bug(ft-create-modules):Fix configuration file

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 from instance.config import app_configuration
 
-db =  SQLAlchemy()
+db = SQLAlchemy()
+
 
 def create_app(config_name):
+    """Creates flask api app."""
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_configuration[config_name])
     app.config.from_pyfile("config.py")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     return app
