889e953c56ec4e19b571ef4812b33a1e3bf4e57a
Beth <bethwambuimuniu@gmail.com>
2017-12-30 22:45:43 +0300

    bug(ft-create-auth-blueprint):Fix models file

--- app/models.py
+++ app/models.py
@@ -1,52 +1,62 @@
-import datetime
+from datetime import datetime, timedelta
 import jwt
-from app import db
+import re
 from flask import current_app
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
+db = SQLAlchemy()
 
 class User(db.Model):
     """User database class."""
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), unique=True)
+    email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
-    events = db.relationship(
-        'Event',
-        backref="users",
-        order_by="Event.id",
-        cascade="all, delete-orphan")
+    # events = db.relationship(
+    #     'Event',
+    #     backref="users",
+    #     lazy="dynamic",
+    #     order_by="Event.id",
+    #     cascade="all, delete-orphan")
 
-    def __init__(name, email, password):
+    def __init__(self, name, email, password):
         self.name = name
+        self.email = email
         self.password = generate_password_hash(password)
 
-    def password_valid(self, password):
+    @staticmethod
+    def validate_email(email):
+        return bool(re.match(r"([\w\.-]+)@([\w\.-]+)(\.[\w\.]+$)", email))
+
+    def validate_password(self, password):
         """Checks the password against the hash password."""
         return check_password_hash(self.password, password)
 
-    def encoding_token(self, user_id):
+    def generate_token(self, user_id):
         """Generate a token for authenication return String."""
         try:
             payload = {
-                "exp": datetime.utnow() + datetime.timedelta(seconds=10),
-                "iat": datetime.datetime.utnow(),
+                "exp": datetime.utcnow() + timedelta(seconds=10),
+                "iat": datetime.utcnow(),
                 "sub": user_id
             }
-            return jwt_encode(
+
+            jwt_string =  jwt.encode(
                 payload,
-                current_app.config.get('SECRET_KEY'),
+                current_app.config['SECRET'],
                 algorithm='HS256')
+            return jwt_string
         except Exception as e:
-            return e
+            return str(e)
 
     @staticmethod
     def decoding_token(token_auth):
         """Decodes a token and return String|Integer."""
         try:
-            payload = jwt.decode(token_auth, app.config.get('SECRET_KEY'))
+            payload = jwt.decode(token_auth, current_app.config['SECRET'])
             return payload['sub']
         except jwt.ExpiredSignatureError:
             return 'Signature expired. Please log in again.'
@@ -58,94 +68,90 @@ class User(db.Model):
         db.session.add(self)
         db.session.commit()
 
-    @staticmethod
-    def get_all_users():
-        """Gets all the users."""
-        return User.query.all()
-
     def __repr__(self):
         """Returns a representaion of the User class instance."""
         return "<User: {}>".format(self.name)
 
-
-class Event(db.Model):
-    """ Event database class."""
-    __tablename__ = "events"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.String(255), nullable=False)
-    category = db.Column(db.String(255), nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
-    author = db.Column(db.String, db.ForeignKey(User.id))
-    location = db.Column(db.String(255), nullable=False)
-    rsvps = db.relationship(
-        'Rsvp',
-        order_by="RSVP.id",
-        backref="events",
-        order_by="Rsvp.id",
-        cascade="all, delete-orphan")
-
-    def __init__(name, description, category, date, author, location):
-        self.name = name
-        self.description = description
-        self.category = category
-        self.date = date
-        self.author = author
-        self.location = location
-
-    def save_event(self):
-        """Addes an event and stores it in the database."""
-        db.session.add(self)
-        db.session.commit()
-
-    @staticmethod
-    def get_all_event(user_id):
-        """Gets all the events according to a particular user."""
-        return Event.query.filter_by(author=user_id).all()
-
-    def delete_event(self):
-        """Deletes an event."""
-        db.session.delete(self)
-        db.session.commit()
-
-    def __repr__(self):
-        """Returns a representaion of the Event class instance."""
-        return "<Event: {}>".format(self.name)
-
-
-class Rsvp(db.Model):
-    """ Rsvp database class"""
-    __tablename__ = "rsvps"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
-    phone_no = db.Column(db.Integer, nullable=False)
-    category = db.Column(db.String(255), nullable=False)
-
-    def __init__(name, email, phone_no, category, rsvp_id):
-        self.name = name
-        self.email = email
-        self.phone_no = phone_no
-        self.category = category
-        self.rsvp_id = self.rsvp_id
-
-    def save_rsvp(self):
-        """Adds a rsvp to the database"""
-        db.session.add(self)
-        db.session.commit()
-
-    @staticmethod
-    def get_all_rsvp(rsvp_id):
-        """Gets all the rsvp according to a particular eventid."""
-        return Rsvp.query.filter_by(rsvp_id=rsvp_id).all()
-
-    def delete_rsvp(self):
-        """Deletes a rsvp of the event."""
-        db.session.delete(self)
-        db.session.commit()
-
-    def __repr__(self):
-        """Returns a representaion of the Rsvp class instance."""
-        return "<Rsvp: {}>".format(self.name)
+#
+# class Event(db.Model):
+#     """ Event database class."""
+#     __tablename__ = "events"
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(255), nullable=False)
+#     description = db.Column(db.String(255), nullable=False)
+#     category = db.Column(db.String(255), nullable=False)
+#     date = db.Column(db.DateTime, nullable=False)
+#     author = db.Column(db.String, db.ForeignKey(User.id))
+#     location = db.Column(db.String(255), nullable=False)
+#     rsvps = db.relationship(
+#         'Rsvp',
+#         backref="events",
+#         order_by="Rsvp.event_id",
+#         cascade="all, delete-orphan")
+#
+#     def __init__(self, name, description, category, date, author, location):
+#         self.name = name
+#         self.description = description
+#         self.category = category
+#         self.date = date
+#         self.author = author
+#         self.location = location
+#
+#     def save_event(self):
+#         """Addes an event and stores it in the database."""
+#         db.session.add(self)
+#         db.session.commit()
+#
+#     @staticmethod
+#     def get_all_event(user_id):
+#         """Gets all the events according to a particular user."""
+#         return Event.query.filter_by(author=user_id).all()
+#
+#     def delete_event(self):
+#         """Deletes an event."""
+#         db.session.delete(self)
+#         db.session.commit()
+#
+#     def __repr__(self):
+#         """Returns a representaion of the Event class instance."""
+#         return "<Event: {}>".format(self.name)
+#
+#
+# class Rsvp(db.Model):
+#     """ Rsvp database class"""
+#     __tablename__ = "rsvps"
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(255), nullable=False)
+#     email = db.Column(db.String(255), nullable=False)
+#     phone_no = db.Column(db.Integer, nullable=False)
+#     event_id = db.Column(db.Integer, db.ForeignKey(Event.id))
+#     category = db.Column(db.String(255), nullable=False)
+#
+#     def __init__(self, name, email, phone_no, category, event_id):
+#         self.name = name
+#         self.email = email
+#         self.phone_no = phone_no
+#         self.category = category
+#         self.event_id = event_id
+#
+#
+#     def save_rsvp(self):
+#         """Adds a rsvp to the database"""
+#         db.session.add(self)
+#         db.session.commit()
+#
+#     @staticmethod
+#     def get_all_rsvp(rsvp_id):
+#         """Gets all the rsvp according to a particular eventid."""
+#         return Rsvp.query.filter_by(rsvp_id=rsvp_id).all()
+#
+#     def delete_rsvp(self):
+#         """Deletes a rsvp of the event."""
+#         db.session.delete(self)
+#         db.session.commit()
+#
+#     def __repr__(self):
+#         """Returns a representaion of the Rsvp class instance."""
+#         return "<Rsvp: {}>".format(self.name)
