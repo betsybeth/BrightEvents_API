90433c1de7caf81995cd9ea162b715a2f1d97d67
Beth <bethwambuimuniu@gmail.com>
2017-12-30 22:52:49 +0300

    bug(ft-create-auth-blueprint):Remove unused code

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask_api import FlaskAPI
-from flask_sqlalchemy import SQLAlchemy
+from .auth.views import auth_blueprint
+from .models import db
+from flask_cors import CORS
 
 from instance.config import app_configuration
 
-db = SQLAlchemy()
 
 
 def create_app(config_name):
     """Creates flask api app."""
     app = FlaskAPI(__name__, instance_relative_config=True)
+    CORS(app)
     app.config.from_object(app_configuration[config_name])
     app.config.from_pyfile("config.py")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.register_blueprint(auth_blueprint)
     db.init_app(app)
     return app
